74af180c8c73e0a16c5d1bea71699a1c9d4c67df
Saulo Braine <saulo@braine.dev>
2021-02-22 21:48:14 -0300

    Aula 1 - Rumo ao próximo nível

---
+++ src/components/ExperienceBar/index.tsx
@@ -0,0 +1,18 @@
+import './styles.css';
+
+const ExperienceBar: React.FC = () => {
+  return (
+    <>
+      <header className="experience-bar">
+        <span>0 xp</span>
+        <div>
+          <div style={{ width: '50%' }} />
+          <span className="current-experience" style={{ left: '50%' }}>300px</span>
+        </div>
+        <span>600 xp</span>
+      </header>
+    </>
+  );
+};
+
+export default ExperienceBar;
